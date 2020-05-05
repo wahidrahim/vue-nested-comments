562dc1084bf2f7e5479b2601b7cb01777e7c37da
Wahid Rahim <wrahim@engagepeople.com>
2020-05-04 20:11:10 -0400

    add avatar component in comment component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="avatar" :style="backgroundImage"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      required: true,
+      type: String,
+    },
+  },
+  computed: {
+    backgroundImage() {
+      return {
+        backgroundImage: `url(${this.url})`,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.avatar {
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  background-size: cover;
+}
+</style>
--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="comment">comment</div>
+  <div class="comment">
+    <Avatar :url="avatarUrl" />
+  </div>
 </template>
 
 <script>
-export default {};
+import Avatar from './Avatar';
+
+import commentData from '../comment-data.json';
+
+export default {
+  components: {
+    Avatar,
+  },
+  computed: {
+    avatarUrl: () => commentData.avatar_url,
+  },
+  created() {
+    console.log(commentData);
+  },
+};
 </script>
 
 <style></style>
