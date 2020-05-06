f886e991d3b59ef01b16a27d3f94a7d4870e00b3
Wahid Rahim <wahidrahim@gmail.com>
2020-05-06 00:38:31 -0400

    update badge spec

--- tests/unit/badge.spec.js
+++ tests/unit/badge.spec.js
@@ -1,8 +1,8 @@
 import { shallowMount } from '@vue/test-utils';
 import Badge from '@/components/Badge';
 
-describe('Avatar.vue', () => {
-  it('has a background image', () => {
+describe('Badge.vue', () => {
+  it('shows text in default slot', () => {
     const badgeText = 'author';
     const wrapper = shallowMount(Badge, {
       slots: {
@@ -11,5 +11,16 @@ describe('Avatar.vue', () => {
     });
 
     expect(wrapper.find('.badge').text()).toMatch(badgeText);
+  });
+
+  it('has a class provided through variant prop', () => {
+    const variant = 'dark';
+    const wrapper = shallowMount(Badge, {
+      propsData: {
+        variant,
+      },
+    });
+
+    expect(wrapper.find(`.badge.${variant}`).element).toBeTruthy();
   });
 });
