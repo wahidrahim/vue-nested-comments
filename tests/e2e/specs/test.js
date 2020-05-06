28875847e8d611037a2d63e5a7bc0df0bec6112f
Wahid Rahim <wrahim@engagepeople.com>
2020-05-06 14:47:22 -0400

    init integration tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,24 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
-    cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+describe('Nested comments demo project', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('shows author name', () => {
+    cy.contains('.user-name', /Brad/i);
+  });
+
+  it('shows author badge', () => {
+    cy.contains('.badge', /AUTHOR/i);
+  });
+
+  it('shows how long ago it was created', () => {
+    cy.contains('.created-at', /.*\d.+ago/gi);
+  });
+
+  it('shows comment', () => {
+    cy.contains(
+      '.comment-body .body',
+      'So what the German automaker is likely to focus on today is the bigger picture. This will be the first time we see the Taycan free from any prototype bodywork.'
+    );
   });
 });
