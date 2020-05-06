033576201830ea991e9f18309b104058512b0a09
Wahid Rahim <wahidrahim@gmail.com>
2020-05-05 20:08:50 -0400

    update comment content component

--- src/components/CommentContent.vue
+++ src/components/CommentContent.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="comment-content">
-    <div class="info">
-      <div class="d-flex-row align-items-center">
-        <span class="user-name font-bold">{{ comment.user_name }}</span>
-        <!-- Re-usable Badge component, as there are usually multiple badge types -->
-        <Badge v-if="comment.user_type" variant="dark">
-          {{ comment.user_type }}
-        </Badge>
-        <span class="created-at font-light">
-          · {{ comment.created_at | timeAgo }}
-        </span>
-      </div>
-
-      <p class="body font-light">{{ comment.body }}</p>
+    <div class="d-flex-row align-items-center">
+      <span class="user-name font-bold">
+        {{ comment.user_name }}
+      </span>
+      <!-- Re-usable Badge component, as there are usually multiple badge types -->
+      <Badge v-if="comment.user_type" variant="dark">
+        {{ comment.user_type }}
+      </Badge>
+      <span class="created-at font-light">
+        {{ comment.created_at | timeAgo }}
+      </span>
     </div>
+
+    <p class="body font-light">{{ comment.body }}</p>
   </div>
 </template>
 
@@ -35,8 +35,8 @@ export default {
 
 <style lang="scss" scoped>
 .comment-content {
-  .user-name {
-    margin-right: 0.64rem;
+  .badge {
+    margin-left: 0.64rem;
   }
 
   .created-at {
@@ -44,6 +44,11 @@ export default {
     color: gray;
     text-transform: uppercase;
     font-size: 1.12rem;
+
+    &:before {
+      content: '•';
+      margin-right: 0.32rem;
+    }
   }
 
   .body {
