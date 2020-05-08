1802e61c50254136c4b65396ed6cd11419b45425
Wahid Rahim <wahidrahim@gmail.com>
2020-05-08 00:00:41 -0400

    add action buttons specs

---
+++ tests/unit/Comment/ActionButtons.spec.js
@@ -0,0 +1,40 @@
+import { shallowMount } from '@vue/test-utils';
+import ActionButtons from '@/components/CommentActions';
+
+describe('CommentActions.vue', () => {
+  it('emits "toggleReplyForm" event', async () => {
+    const wrapper = shallowMount(ActionButtons, {
+      propsData: {
+        replies: [],
+        votes: {
+          up: 123,
+          down: 0,
+        },
+      },
+    });
+
+    wrapper.vm.$emit('toggleReplyForm');
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted().toggleReplyForm).toBeTruthy();
+  });
+
+  it('emits "toggleReplies" event', async () => {
+    const wrapper = shallowMount(ActionButtons, {
+      propsData: {
+        replies: [],
+        votes: {
+          up: 123,
+          down: 0,
+        },
+      },
+    });
+
+    wrapper.vm.$emit('toggleReplies');
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted().toggleReplies).toBeTruthy();
+  });
+});
